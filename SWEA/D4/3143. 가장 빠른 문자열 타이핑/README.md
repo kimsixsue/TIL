f06a55996b871beaaef9de6af3beb1b66976d334
김진욱 <cjg03217@gmail.com>
2022-11-06 13:48:17 +0900

    [D4] Title: 가장 빠른 문자열 타이핑, Time: 110 ms, Memory: 54,612 KB -BaekjoonHub

---
+++ SWEA/D4/3143. 가장 빠른 문자열 타이핑/README.md
@@ -0,0 +1,11 @@
+# [D4] 가장 빠른 문자열 타이핑 - 3143 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV_65wkqsb4DFAWS) 
+
+### 성능 요약
+
+메모리: 54,612 KB, 시간: 110 ms, 코드길이: 482 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/3143. 가장 빠른 문자열 타이핑/가장 빠른 문자열 타이핑.py
@@ -0,0 +1,14 @@
+T = int(input())  # 테스트 케이스의 수 T
+for testcase in range(1, T + 1):
+    A, B = input().split()  # 두 문자열 A, B
+    len_a = len(A)  # A의 길이 1이상 10000이하
+    len_b = len(B)  # B의 길이 1이상   100이하
+    i = count = 0
+    while i < len_a:
+        if A[i: i + len_b] == B:
+            i += len_b
+        else:
+            i += 1
+        count += 1
+    # A 전체를 타이핑 하기 위해 키를 눌러야 하는 횟수의 최솟값을 출력한다.
+    print(f'#{testcase} {count}')  # 1 ~ 10000
