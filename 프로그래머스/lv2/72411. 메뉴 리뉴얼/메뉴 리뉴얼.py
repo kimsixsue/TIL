3c2dd67015eded3add9e48f4d6a4cf2acc8d1eaa
김진욱 <cjg03217@gmail.com>
2022-11-06 14:11:34 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 0.30 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -0,0 +1,38 @@
+from itertools import combinations
+
+
+def solution(orders, course):
+    answer = list()
+    course.sort(reverse=True)
+    for cour in course:
+        candidates = dict()
+        for order in orders:
+            order = sorted(order)
+            for comb in combinations(order, cour):
+                dish = ''.join(sorted(comb))
+                if candidates.get(dish):
+                    candidates[dish] += 1
+                else:
+                    candidates[dish] = 1
+        if candidates:
+            count = max(candidates.values())
+            if 2 <= count:
+                for k, v in candidates.items():
+                    if v == count:
+                        answer.append(k)
+    answer.sort()
+    return answer
+
+
+orders = ["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"]
+course = [2, 3, 4]
+print(solution(orders, course))
+orders = ["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"]
+course = [2, 3, 5]
+print(solution(orders, course))
+orders = ["XYZ", "XWY", "WXA"]
+course = [2, 3, 4]
+print(solution(orders, course))
+orders = ["ABCD", "ABCD", "ABCD"]
+course = [2, 3, 4]
+print(solution(orders, course))
