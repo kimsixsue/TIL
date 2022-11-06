9aa6e5e49d7d7457a93ac7886e5f46c96150e84e
김진욱 <cjg03217@gmail.com>
2022-11-06 13:37:31 +0900

    [Silver I] Title: 맥주 마시면서 걸어가기, Time: 100 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -0,0 +1,30 @@
+import sys
+
+input = sys.stdin.readline
+t = int(input())  # testcase number
+for _ in range(t):
+    n = int(input())  # CVS number
+    x, y = [[] for _ in range(n + 2)], [[] for _ in range(n + 2)]
+    for _ in range(n + 2):  # (x, y) * (n+2)
+        x[_], y[_] = map(int, input().split())
+    # find edge
+    edge = [[] for _ in range(n + 2)]
+    for a in range(n + 2):
+        for b in range(a + 1, n + 2):
+            if abs(x[b] - x[a]) + abs(y[b] - y[a]) <= 1000:
+                edge[a].append(b)
+                edge[b].append(a)
+    # DFS (index: 0 -> n+1)
+    visited = [False] * (n + 2)
+    visited[0] = True
+    queue = [0]
+    while queue:
+        q = queue.pop()
+        for nxt in edge[q]:
+            if not visited[nxt]:
+                visited[nxt] = True
+                queue.append(nxt)
+    if visited[-1]:
+        print('happy')
+    else:
+        print('sad')
